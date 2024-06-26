4dbf98e8ed2231187bbcb913704aa2e47e675198
Bryan Santana <bryansantana0210@gmail.com>
2024-06-26 15:02:01 -0400

    Working to fix a few bugs

--- Database Work/get_player_team_history.py
+++ Database Work/get_player_team_history.py
@@ -40,13 +40,11 @@ def get_team_history(player_id):
 
     # Extract information for each roster entry
     roster_entries = response['people'][0]['rosterEntries']
-    print(roster_entries)
     team_history = []
     
     team_ids = get_team_ids()
     
     for entry in roster_entries:
-        print(entry)
         if entry['team']['id'] in team_ids:
             team_entry = (
                 entry['team']['name'],        # Team Name
@@ -59,7 +57,7 @@ def get_team_history(player_id):
     return team_history
 
 
-def get_teammates_by_team_history(team_history, teammates, cursor):
+def get_teammates_by_team_history(team_history, teammates, cursor, con):
     """
     Get an MLB player's teammates given a team_history, which contains team name, team ID, start date, and end date
     Returns a set of unique player names for which the player played with in his career.
@@ -77,55 +75,46 @@ def get_teammates_by_team_history(team_history, teammates, cursor):
         date_range = [start_date + datetime.timedelta(days=x) for x in range((end_date-start_date).days + 1)]
 
         for date in date_range:
-            team_roster = get_daily_active_roster_as_set(team_id, date, teammates)
+            team_roster = get_daily_active_roster_as_set(team_id, date, teammates, cursor, con)
             for player in team_roster:
                 new_teammates = team_roster.difference(teammates[player])
-                update_player_teammates(date, player, new_teammates, cursor)
+                update_player_teammates(date, player, new_teammates, cursor, con)
                 new = teammates[player].union(team_roster)
                 teammates[player] = new
     return teammates
 
-def update_player_teammates(date, player_name, teammates_to_add, cursor):
+def update_player_teammates(date, player_name, teammates_to_add, cursor, con):
     player_query = (player_name, date.year)
     player_id = get_player_id(player_query)
-    for teammate in teammates_to_add:
-        player_query = (teammate, date.year)
-        teammate_id = get_player_id(player_query)
-        cursor.execute ("INSERT INTO Teammates (player_id, teammate_id) VALUES (%s, %s)", (player_id, teammate_id))
-        con.commit()
+    if player_id:
+        for teammate in teammates_to_add:
+            player_query = (teammate, date.year)
+            teammate_id = get_player_id(player_query)
+            if teammate_id:
+                cursor.execute ("INSERT INTO Teammates (player_id, teammate_id) VALUES (%s, %s)", (player_id, teammate_id))
+                con.commit()
 
 
 def get_player_id(name_query):
     player_return = statsapi.lookup_player(name_query[0], gameType="R", season=name_query[1], sportId=1)
+    if len(player_return) == 0:
+        return None
     #If only one unique player is returned from the query
-    if len(player_return) == 1:
+    elif len(player_return) == 1:
         return player_return[0]['id']
     #If multiple players are returned by the query, ie same name, have the user manually select which one they want
     else:
         for index, entry in enumerate(player_return):
             print("Player at index", index, " : ", entry)
-        index_entry = int(input(("Type the index of the player you want:")))
+        index_entry = int(input(("Type the index of the player you want: (looking for: )", name_query)))
         return player_return[index_entry]['id']
 
 
-def get_daily_active_roster_as_set(team_id, date, teammates, cursor):
+def get_daily_active_roster_as_set(team_id, date, teammates, cursor, con):
     team_roster = set()
     formatted_date = date.strftime("%m/%d/%Y")
     roster = (statsapi.roster(team_id, date=formatted_date, rosterType="active"))
     roster = roster.split()
-    team_string = team_id + "-" + date.year()
-
-    cursor.execute("INSERT INTO Teams (team_name) VALUES (%s) ON CONFLICT (team_name) DO NOTHING RETURNING team_id;", (team_string,))
-    generated_team_id = cursor.fetchone()
-    # If no team_id was returned, select the existing team_id
-    if generated_team_id is None:
-        cursor.execute("SELECT team_id FROM Teams WHERE team_name = %s;", (team_string,))
-        generated_team_id = cursor.fetchone()[0]
-    else:
-        generated_team_id = generated_team_id[0]
-    print("Added", team_string, "to the Teams database")
-    con.commit()
-
     x = 2
     while x < len(roster):
         if x + 2 < len(roster) and '#' not in roster[x + 2]:
@@ -143,33 +132,46 @@ def get_daily_active_roster_as_set(team_id, date, teammates, cursor):
                     teammates[player_string] = set()
                     player_query = (player_string, date.year)
                     player_id = get_player_id(player_query)
-                    cursor.execute("INSERT INTO MLB_Players (player_id, name, position, number) VALUES (%s, %s, %s, %s)", (player_id, player_string, player_position, player_number))
-                    con.commit()
-                    cur.execute("INSERT INTO Player_Team_Join (player_id, team_id, start_date, end_date) VALUES (%s, %s, %s, %s) ON CONFLICT (player_id, team_id) DO UPDATE SET end_date = EXCLUDED.end_date;", (player_id, team_id, date, date))
-                    print("Added", player_id, player_string, "played on ", team_id, "on", date, "to the database")
-                    print("Added", player_id, player_string, player_position, player_number, "to the database")
+                    if player_id:
+                        cursor.execute("INSERT INTO MLB_Players (player_id, name, position, number) VALUES (%s, %s, %s, %s) ON CONFLICT DO NOTHING", (player_id, player_string, player_position, player_number))
+                        con.commit()
+                        print("Added", player_id, player_string, player_position, player_number, "to the database")
     return team_roster
             
 
-def fill_teammates_for_season(opening_day, last_day, teammates, players_checked, cursor):
+def fill_teammates_for_season(opening_day, last_day, teammates, players_checked, cursor, con):
     date_range = [opening_day + datetime.timedelta(days = x) for x in range((last_day - opening_day).days + 1)]
     team_ids = get_team_ids()
     for date in date_range:
         for team_id in team_ids:
-           print("Checking", team_id, "on ", date, "at ", datetime.datetime.now())
-           daily_roster = get_daily_active_roster_as_set(team_id, date, teammates, cursor)
-           for player in daily_roster:
-               if player not in players_checked:
-                players_checked.add(player)
-                print("Checking", player, "at " , datetime.datetime.now())
-                player_query = (
-                   player,
-                   date.year
-               )
-                id = get_player_id(player_query)
-                team_history = get_team_history(id)
-                teammates = get_teammates_by_team_history(team_history, teammates, cursor)
-                print("Finished", player, "at ", datetime.datetime.now())
+            team_string = str(team_id) + "-" + str(date.year)
+            cursor.execute("INSERT INTO Teams (team_name) VALUES (%s) ON CONFLICT (team_name) DO NOTHING RETURNING team_id;", (team_string,))
+            generated_team_id = cursor.fetchone()
+        # If no team_id was returned, select the existing team_id
+            if generated_team_id is None:
+                cursor.execute("SELECT team_id FROM Teams WHERE team_name = %s;", (team_string,))
+                generated_team_id = cursor.fetchone()[0]
+            else:
+                generated_team_id = generated_team_id[0]
+            print("Added", team_string, "to the Teams database")
+            con.commit()
+            print("Checking", team_id, "on ", date, "at ", datetime.datetime.now())
+            daily_roster = get_daily_active_roster_as_set(team_id, date, teammates, cursor, con)
+            for player in daily_roster:
+                if player not in players_checked:
+                    players_checked.add(player)
+                    print("Checking", player, "at " , datetime.datetime.now())
+                    player_query = (
+                    player,
+                    date.year
+                )
+                    player_id = get_player_id(player_query)
+                    if player_id:
+                        cursor.execute("INSERT INTO Player_Team_Join (player_id, team_id, start_date, end_date) VALUES (%s, %s, %s, %s) ON CONFLICT (player_id, team_id) DO UPDATE SET end_date = EXCLUDED.end_date;", (player_id, generated_team_id, date, date))
+                        print("Added", player_id, player, "played on ", generated_team_id, "on", date, "to the database")
+                        team_history = get_team_history(player_id)
+                        teammates = get_teammates_by_team_history(team_history, teammates, cursor, con)
+                    print("Finished", player, "at ", datetime.datetime.now())
     return teammates, players_checked
 
 def populate_database ():
@@ -186,8 +188,10 @@ def populate_database ():
         valid_dates = cur.fetchall() 
         opening_day = valid_dates[0][0]
         last_day = valid_dates[0][1]
-        fill_teammates_for_season(opening_day, last_day,teammates, players_checked, cur)
+        fill_teammates_for_season(opening_day, last_day,teammates, players_checked, cur, con)
         season -= 1
     con.commit()  # Commit the transaction to save the changes\
+#populate_database()
 
-populate_database()
+#query = ("Zach Jackson", 2024)
+#print(get_player_id(query))
